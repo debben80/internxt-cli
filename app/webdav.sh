0973192b7d35962a599714d12b27d1ebb9b64291
debben80 <30798457+debben80@users.noreply.github.com>
2025-08-31 22:17:26 +0200

    Update webdav.sh
    force reexecution with appuser

--- app/webdav.sh
+++ app/webdav.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 . /app/functions.sh
+spath="$(readlink -f "$0")"
+if [ "$(whoami)" = "root" ]; then
+  exec su-exec appuser "$spath" "$@"
+fi
 
 WEBDAV_PROTO=${WEBDAV_PROTO:-https}
 WEBDAV_PORT=${WEBDAV_PORT:-3005}
