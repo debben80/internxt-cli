b512b1adbdfba469e5eff9cb7009e80a320ba607
debben80 <30798457+debben80@users.noreply.github.com>
2025-08-31 22:15:53 +0200

    Update healthcheck.sh

--- app/healthcheck.sh
+++ app/healthcheck.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 set -e
-
-if [ -f "/app/webdav" ]; then
-    internxt webdav status || kill 1
-else
-    exit 0
+spath="$(readlink -f "$0")"
+if [ "$(whoami)" = "root" ]; then
+  exec su-exec appuser "$spath" "$@"
 fi
+
+internxt whoami | grep -q "$INTERNXT_EMAIL" && echo "logged with $INTERNXT_EMAIL" || exit 1
+if [ -f "/app/webdav" ]; then
+    internxt webdav status | grep -q "online" && echo "WebDAV server online" || exit 1
+fi
+
+exit 0
