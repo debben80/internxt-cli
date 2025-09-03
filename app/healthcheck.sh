d339516df63eb6dfc7ab1801928ffe2673203c57
bde <benoit.debure@staff.win.be>
2025-09-03 11:20:38 +0200

    update healthcheck

--- app/healthcheck.sh
+++ app/healthcheck.sh
@@ -9,18 +9,14 @@ fi
 
 # Internxt Login Check
 internxt_whoami=$(internxt whoami --json | jq -r '.login.user.username')
-if [ "$internxt_whoami" = "$INTERNXT_EMAIL" ]; then
-  echo "logged with $INTERNXT_EMAIL"
-else
+if [ "$internxt_whoami" != "$INTERNXT_EMAIL" ]; then
   exit 1
 fi
 
 # WebDAV Server Status Check
 if [ -f "/app/webdav.mode" ]; then
-  internxt_webdav_status=$(internxt webdav status --json | jq -r '.success')
-  if [ "$internxt_webdav_status" = "true" ]; then
-    echo "WebDAV server online"
-  else
+  internxt_webdav_status=$(internxt webdav status --json | jq -r '.message')
+  if [ "${internxt_webdav_status#*: }" != "online" ]; then
     exit 1
   fi
 fi
