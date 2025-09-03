049d501ee87c11d4baa9ba94017d203322f89665
bde <benoit.debure@staff.win.be>
2025-09-03 10:17:13 +0200

    use json for healthcheck

--- app/healthcheck.sh
+++ app/healthcheck.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 set -e
+
+# Privilege Dropping
 spath="$(readlink -f "$0")"
 if [ "$(whoami)" = "root" ]; then
   exec su-exec appuser "$spath" "$@"
 fi
 
-internxt whoami | grep -q "$INTERNXT_EMAIL" && echo "logged with $INTERNXT_EMAIL" || exit 1
-if [ -f "/app/webdav" ]; then
-    internxt webdav status | grep -q "online" && echo "WebDAV server online" || exit 1
+# Internxt Login Check
+internxt_whoami=$(internxt whoami --json | jq -r '.login.user.username')
+if [ "$internxt_whoami" = "$INTERNXT_EMAIL" ]; then
+  echo "logged with $INTERNXT_EMAIL"
+else
+  exit 1
+fi
+
+# WebDAV Server Status Check
+if [ -f "/app/webdav.mode" ]; then
+  internxt_webdav_status=$(internxt webdav status --json | jq -r '.success')
+  if [ "$internxt_webdav_status" = "true" ]; then
+    echo "WebDAV server online"
+  else
+    exit 1
+  fi
 fi
 
 exit 0
